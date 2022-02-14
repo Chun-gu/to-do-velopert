87714c75d5c635434dbc4a17109102b24413252c
Chun-gu <chungu.yi92@gmail.com>
2022-02-14 23:11:54 +0900

    ✨ TodoItem 기능 구현

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,16 +1,24 @@
+import React from "react";
 import { TodoItem, CheckCircle, Text, Remove } from "./styled";
 import { MdDone, MdDelete } from "react-icons/md";
+import useTodoDispatch from "../../hook/useTodoDispatch";
 
-const index = ({ id, done, text }) => {
+const Index = ({ id, done, text }) => {
+  const dispatch = useTodoDispatch();
+  const onToggle = () => dispatch({ type: "TOGGLE", id });
+  const onRemove = () => dispatch({ type: "REMOVE", id });
+
   return (
     <TodoItem>
-      <CheckCircle done={done}>{done && <MdDone />}</CheckCircle>
+      <CheckCircle done={done} onClick={onToggle}>
+        {done && <MdDone />}
+      </CheckCircle>
       <Text done={done}>{text}</Text>
-      <Remove>
+      <Remove onClick={onRemove}>
         <MdDelete />
       </Remove>
     </TodoItem>
   );
 };
 
-export default index;
+export default React.memo(Index);
